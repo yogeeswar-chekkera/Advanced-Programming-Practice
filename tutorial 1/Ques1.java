fc7374d6001076dd9c18022432b30a67718a7145
Yogeeswar Chekkera <yogeeswarchekkera@gmail.com>
2026-07-30 19:49:40 +0530

    Initial commit

---
+++ tutorial 1/Ques1.java
@@ -0,0 +1,17 @@
+public class StudentInformation {
+public static void main(String[] args) {
+// Student Details
+String studentName = "Yogeeswar";
+String registerNumber = "RA2511030010305";
+String department = "Computer Science and Engineering";
+int yearOfStudy = 2;
+String collegeName = "SRM Engineering College";
+// Display Student Information
+System.out.println("STUDENT INFORMATION");
+System.out.println("Student Name      : " + studentName);
+System.out.println("Register Number   : " + registerNumber);
+System.out.println("Department        : " + department);
+System.out.println("Year of Study     : " + yearOfStudy);
+System.out.println("College Name      : " + collegeName);
+}
+}
